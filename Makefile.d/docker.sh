d6acc761507bb3e21322631b33cbf91861a64f16
Niklas Metje <22395665+niklasmtj@users.noreply.github.com>
2022-12-16 12:19:57 +0100

    Feature: Build multiarch with Docker buildx
    Signed-off-by: Niklas Metje <22395665+niklasmtj@users.noreply.github.com>

--- Makefile.d/docker.sh
+++ Makefile.d/docker.sh
@@ -27,7 +27,13 @@ function build() {
           IMAGE_TAG="${IMAGE_TAG}-${STAGE}"
         fi
 
-        DOCKER_BUILDKIT=1 docker build --build-arg VERSION=${VERSION} --target ${1}-${STAGE} -t ${DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}:${IMAGE_TAG}-${LINUX_DISTRIBUTION} -f ${CONTEXT}${LINUX_DISTRIBUTION}.Dockerfile ${CONTEXT}
+        docker buildx build \
+        --push \
+        --platform="linux/amd64,linux/arm64" \
+        --build-arg VERSION="${VERSION}" \
+        --target "${1}-${STAGE}" \
+        -t "${DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}:${IMAGE_TAG}-${LINUX_DISTRIBUTION}" \
+        -f "${CONTEXT}${LINUX_DISTRIBUTION}.Dockerfile" "${CONTEXT}"
       done
     done
   done
